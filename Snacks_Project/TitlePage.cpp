943caf0f3b7751b42cf6cc80ebbeb021d8bc79a4
6720 <6720@ait-sydney.net.au>
2018-08-30 15:36:29 +1000

    text added

---
+++ Snacks_Project/TitlePage.cpp
@@ -0,0 +1,11 @@
+#include "TitlePage.h"
+
+
+TitlePage::TitlePage()
+{
+}
+
+
+TitlePage::~TitlePage()
+{
+}
--- Snacks_Project/main.cpp
+++ Snacks_Project/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <SDL_ttf.h>
 
+
 using namespace std;
 
 SDL_Window* window = NULL;
@@ -69,7 +70,7 @@ int main(int argc, char **argv)
 
 	//Load up our font
 	//								font file path				font size
-	TTF_Font* font = TTF_OpenFont("assets/vermin_vibes_1989.ttf", 50);
+	TTF_Font* font = TTF_OpenFont("assets/vermin_vibes_1989.ttf", 100);
 	//create a colour for our text
 	SDL_Color textcolour = { 0, 0, 0, 255 }; //RGBA
 	//Create surface using font, colour and desired output text
@@ -81,14 +82,69 @@ int main(int argc, char **argv)
 
 	//setup rectangle to describe where to draw this text
 	SDL_Rect textDestination;
-	textDestination.x = 150;
+	textDestination.x = 92;
 	textDestination.y = 100;
 
 	//to get the width and height, query the surface
 	SDL_QueryTexture(textTexture, NULL, NULL, &textDestination.w, &textDestination.h);
 
+	//TEXT: Play Game*****************************************************************
+	TTF_Font* font1 = TTF_OpenFont("assets/BebasNeueBold.ttf", 45);
+	//create a colour for our text
+	SDL_Color textcolour1 = { 0, 0, 0, 255 }; //RGBA
+	//Create surface using font, colour and desired output text
+	SDL_Surface* textSurface1 = TTF_RenderText_Blended(font1, "Play Game", textcolour1);
+	//convert suface to texture
+	SDL_Texture* textTexture1 = SDL_CreateTextureFromSurface(renderer, textSurface1);
+	//don't need the surface no more
+	SDL_FreeSurface(textSurface1);
+
+	//setup rectangle to describe where to draw this text
+	SDL_Rect textDestination1;
+	textDestination1.x = 180;
+	textDestination1.y = 200;
+
+	//to get the width and height, query the surface
+	SDL_QueryTexture(textTexture1, NULL, NULL, &textDestination1.w, &textDestination1.h);
+
+	//TEXT: Instruction*****************************************************************
+	TTF_Font* font2 = TTF_OpenFont("assets/BebasNeueBold.ttf", 45);
+	//create a colour for our text
+	SDL_Color textcolour2 = { 0, 0, 0, 255 }; //RGBA
+	//Create surface using font, colour and desired output text
+	SDL_Surface* textSurface2 = TTF_RenderText_Blended(font2, "Instruction", textcolour2);
+	//convert suface to texture
+	SDL_Texture* textTexture2 = SDL_CreateTextureFromSurface(renderer, textSurface2);
+	//don't need the surface no more
+	SDL_FreeSurface(textSurface2);
+
+	//setup rectangle to describe where to draw this text
+	SDL_Rect textDestination2;
+	textDestination2.x = 180;
+	textDestination2.y = 270;
+
+	//to get the width and height, query the surface
+	SDL_QueryTexture(textTexture2, NULL, NULL, &textDestination2.w, &textDestination2.h);
 
 
+	//TEXT: Exit*****************************************************************
+	TTF_Font* font3 = TTF_OpenFont("assets/BebasNeueBold.ttf", 45);
+	//create a colour for our text
+	SDL_Color textcolour3 = { 0, 0, 0, 255 }; //RGBA
+	//Create surface using font, colour and desired output text
+	SDL_Surface* textSurface3 = TTF_RenderText_Blended(font3, "Exit", textcolour3);
+	//convert suface to texture
+	SDL_Texture* textTexture3 = SDL_CreateTextureFromSurface(renderer, textSurface3);
+	//don't need the surface no more
+	SDL_FreeSurface(textSurface3);
+
+	//setup rectangle to describe where to draw this text
+	SDL_Rect textDestination3;
+	textDestination3.x = 180;
+	textDestination3.y = 340;
+
+	//to get the width and height, query the surface
+	SDL_QueryTexture(textTexture3, NULL, NULL, &textDestination3.w, &textDestination3.h);
 	
 
 
@@ -117,7 +173,10 @@ int main(int argc, char **argv)
 
 
 		//render textTexture
-		SDL_RenderCopy(renderer, textTexture, NULL, &textDestination);
+		SDL_RenderCopy(renderer, textTexture, NULL, &textDestination); //SNACKS
+		SDL_RenderCopy(renderer, textTexture1, NULL, &textDestination1); //play game
+		SDL_RenderCopy(renderer, textTexture2, NULL, &textDestination2); //Instruction
+		SDL_RenderCopy(renderer, textTexture3, NULL, &textDestination3); //Exit
 
 		//when done drawing, present all our renderings to the window
 		SDL_RenderPresent(renderer);
